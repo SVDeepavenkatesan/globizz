947b86150e75b335cd0340921f510c48d209a2ac
KUMARAN CAROUNANIDY <svdeepa03@gmail.com>
2026-03-10 16:55:17 +0530

    Updated home page with events brochure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/home/Hero";
 import Highlights from "../components/home/Highlights";
 import Invitation from "./Invitation";
+import Brochure from "./Brochure";
 const Home = () => {
   return (
       <div className="relative z-10">
         <Hero />
         <Invitation />
+        <Brochure />
         <Highlights />
       </div>
   );
