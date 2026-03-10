e6ecd20e372dadd07613cfce843f1aabfb568af4
KUMARAN CAROUNANIDY <svdeepa03@gmail.com>
2026-03-10 16:18:22 +0530

    Updated home page url tree

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "react-pdf/dist/Page/TextLayer.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/globizz">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
